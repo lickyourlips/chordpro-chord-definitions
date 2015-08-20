b62d0f151ed6e4173771465b1d15365bbb956e43
lickyourlips <tpm.moore@gmail.com>
2015-08-20 11:28:14 +0100

    Initial commit jiggery-pokery

--- PopClip.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'src/autoload.php';
-
-use lickyourlips\ChordProDefinitions\ChordProDefinitions;
-
-$chordPro = getenv('POPCLIP_TEXT');
-$chordProDefs = new ChordProDefinitions($chordPro);
-echo $chordProDefs->getChordProDefsString('guitar');
--- README.md
+++
@@ -1,4 +0,0 @@
-ChordPro Chord Definitions
-==========================
-
-A basic PopClip Extension for ChordPro enthusiasts, to transform chords from a ChordPro file into a list of chord definition templates.
